b57675cc03bdee89b943d2f59d8c339a145acce6
pavan kumar <pavan.yalamanchi@gmail.com>
2021-07-03 17:31:06 +0100

    updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ window.onload = function() {
 }
 
 const cardFetch = async function() {
-    let categories = ['Trending Now', 'Watch It Again', 'New Releases']
+    let categories = ['Trending Now', 'Watch It Again', 'New Releases', 'Horror']
     for (let i = 0; i < categories.length; i++) {
         const url = `https://striveschool-api.herokuapp.com/api/movies/${categories[i]}`
 
@@ -18,15 +18,15 @@ const cardFetch = async function() {
         let cardSection = document.getElementsByClassName('card-section')[0]
 
         cardSection.innerHTML += `<div class="section-title">
-        <div class="text-ellipsis d-inline-block">
-            <h4 class="text-white mb-3 netflix-font first-h4">${categories[i]}</h4>
+        <div class="text-ellipsis d-inline-block mt-4">
+            <h4 class="text-white netflix-font first-h4">${categories[i]}</h4>
         </div>
         </div>`
         let div = document.createElement('div')
         div.classList.add('row', 'mx-n1', 'row-cols-1', 'row-cols-sm-2', 'row-cols-md-3', 'row-cols-lg-4', 'row-cols-xl-5')
         cardSection.appendChild(div)
         for (let j = 0; j < resp.length; j++) {
-            div.innerHTML += `<a href='edit_office.html?cat=${resp[j].category}&loop=${j}&id=${resp[j]._id}'><div class=" col px-1 ">
+            div.innerHTML += `<a href='edit_office.html?cat=${resp[j].category}&loop=${j}&id=${resp[j]._id}'><div class=" col px-1 zoom">
             <div class="position-relative ">
                 <img src="${resp[j].imageUrl} " class="img-fluid rounded mb-2 mb-sm-0 w-100 " alt=" " />
             </div>
